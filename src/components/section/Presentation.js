307360ce40d2f3c347df020ec4e4147406f0d249
Cauê Takeuchi Romano <135703216+Kaue-com-C@users.noreply.github.com>
2024-01-24 01:04:03 +0900

    main

--- src/components/section/Presentation.js
+++ src/components/section/Presentation.js
@@ -6,11 +6,12 @@ function Presentation(){
         <div id='presentation' className={styles.presentation}>
             <h1>Olá, eu sou o Cauê</h1>
             <h2>Bem-vinde ao meu Portfólio</h2>
-            <p> Sou apaixonado por tecnologia e possibilidades de sempre aprender coisas novas que ela proporciona.<br/>
-                Com alguns anos de experiência em gestão de pessoas na área de telecomunicações e suporte e minhas <br/>
-                habilidades na área de tecnologia, posso oferecer soluções criativas e efetivas para problemas complexos e te <br/>
-                ajudar a ter resultados fantásticos para o seu negócio. Conte comigo para subir a receita anual de <br/>
-                sua empresa, estou sempre em busca de novos desafios para superar.
+            <p> Sou fascinado pelo mundo da tecnologia e as infinitas possibilidades de <br/>
+                aprendizado e melhorias queela proporciona.<br/>
+                Com alguns anos de experiência em gestão de pessoas na área de telecomunicações<br/>
+                e suporte e minhas habilidades na área de tecnologia, posso oferecer soluções criativas<br/>
+                e efetivas para problemas complexos e te ajudar a ter resultados fantásticos para o seu negócio.<br/>
+                Conte comigo para subir a receita anual de sua empresa, estou sempre em busca de novos desafios para superar.
             </p>
         <ButtonA link='https://github.com/Kaue-com-C' text='Conecte-se Comigo!!'/>
 
